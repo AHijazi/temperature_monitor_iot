1050d990335a646e54628d90b2ea72dd54282f38
AHijazi <AHijazi@users.noreply.github.com>
2016-08-10 10:28:56 +0400

    Add files via upload

---
+++ AzureIoTHub.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+
+static class AzureIoTHub
+{
+    //
+    // Note: this connection string is specific to the device "myIoTdevidce". To configure other devices,
+    // see information on iothub-explorer at http://aka.ms/iothubgetstartedVSCS
+    //
+    const string deviceConnectionString = "HostName=IoT-temperature1.azure-devices.net;DeviceId=myIoTdevidce;SharedAccessKey=TGdKDSqdddzf3fq9vPPtOGHPrPdOFtcpdiz8W4kiSZU=";
+
+    //
+    // To monitor messages sent to device "myIoTdevidce" use iothub-explorer as follows:
+    //    iothub-explorer HostName=IoT-temperature1.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=mZV444b6fvjvEZRpedv4gUtDE7PUhp5SJqtLyK5esfQ= monitor-events "myIoTdevidce"
+    //
+
+    // Refer to http://aka.ms/azure-iot-hub-vs-cs-wiki for more information on Connected Service for Azure IoT Hub
+
+    public static async Task SendDeviceToCloudMessageAsync(int reportnumber, float temp, float pressure)
+    {
+        var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Amqp);
+        int deviceId = 1;
+        var resultobject = new
+        {
+            count = reportnumber,
+            mDeviceId = deviceId,
+            mTemprature = temp,
+            mPressure = pressure
+            
+        };
+
+        //#if WINDOWS_UWP
+        //        var str = mString ;
+        //#else
+        //        var str = "Hello, Cloud from a C# app!";
+        //#endif
+        var reslutmessage = JsonConvert.SerializeObject(resultobject);
+        var message = new Message(Encoding.ASCII.GetBytes(reslutmessage));
+
+        await deviceClient.SendEventAsync(message);
+    }
+
+    public static async Task<string> ReceiveCloudToDeviceMessageAsync()
+    {
+        var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Amqp);
+
+        while (true)
+        {
+            var receivedMessage = await deviceClient.ReceiveAsync();
+
+            if (receivedMessage != null)
+            {
+                var messageData = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+                await deviceClient.CompleteAsync(receivedMessage);
+                return messageData;
+            }
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Collections.Generic;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Threading;
+using System.Threading.Tasks;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Lesson_203
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        BMP280 BMP280;
+        float temp;
+        float pressure;
+        float altitude;
+        const float seaLevelPressure = 1013.25f;
+        int reportnumber;
+        DispatcherTimer dispatcherTimer;
+
+
+        string resultxt;
+
+        //A class which wraps the barometric sensor
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+             dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 30);
+
+            dispatcherTimer.Tick += tick;
+
+            dispatcherTimer.Start();
+
+       
+
+        }
+
+        private void tick(object sender, object e)
+        {
+            check();
+           
+        }
+
+     
+
+        //This method will be called by the application framework when the page is first loaded
+        protected override async void OnNavigatedTo(NavigationEventArgs navArgs)
+        {
+            Debug.WriteLine("MainPage::OnNavigatedTo");
+
+            MakePinWebAPICall();
+            try
+            {
+                //Create a new object for our barometric sensor class
+                BMP280 = new BMP280();
+                //Initialize the sensor
+                await BMP280.Initialize();
+
+                //Create variables to store the sensor data: temperature, pressure and altitude. 
+                //Initialize them to 0.
+                temp = 0;
+                pressure = 0;
+                altitude = 0;
+
+
+                //Create a constant for pressure at sea level. 
+                //This is based on your local sea level pressure (Unit: Hectopascal)              
+                //Read 10 samples of the data
+                for (int i = 0; i < 10; i++)
+                {
+                    temp = await BMP280.ReadTemperature();
+                    pressure = await BMP280.ReadPreasure();
+                    altitude = await BMP280.ReadAltitude(seaLevelPressure);
+
+
+                    //Write the values to your debug console                            
+                    Debug.WriteLine("Temprature: " + temp.ToString() + " deg");
+                    Debug.WriteLine("Pressure: " + pressure.ToString() + " Pa");
+                    Debug.WriteLine("Altitude: " + altitude.ToString() + "m");
+
+                    await Task.Delay(1000);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+
+
+
+      
+
+
+    }
+       
+
+           /// <summary>
+        // This method will put your pin on the world map of makers using this lesson.
+        // This uses imprecise location (for example, a location derived from your IP 
+        // address with less precision such as at a city or postal code level). 
+        // No personal information is stored.  It simply
+        // collects the total count and other aggregate information.
+        // http://www.microsoft.com/en-us/privacystatement/default.aspx
+        // Comment out the line below to opt-out
+        /// </summary>
+        public void MakePinWebAPICall()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+
+                // Comment this line to opt out of the pin map.
+                client.GetStringAsync("http://adafruitsample.azurewebsites.net/api?Lesson=203");
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Web call failed: " + e.Message);
+            }
+        }
+        private async void check()
+        {
+
+            if (reportnumber == null) reportnumber = 0;
+            else
+                reportnumber = reportnumber+1;
+
+                temp = await BMP280.ReadTemperature();
+                pressure = await BMP280.ReadPreasure();
+                altitude = await BMP280.ReadAltitude(seaLevelPressure);
+
+           
+                 //Random rand = new Random();
+                 //temp = (float)(24 + rand.NextDouble() * 4 - 2);
+                 //pressure = (float)(9800 + rand.NextDouble() * 4 - 2);
+                 //altitude = (float)(110 + rand.NextDouble() * 4 - 2);
+         
+
+            //Write the values to your debug console                            
+            Debug.WriteLine("Temprature: " + temp.ToString() + " deg");
+            Debug.WriteLine("Pressure: " + pressure.ToString() + " Pa");
+            Debug.WriteLine("Altitude: " + altitude.ToString() + "m");
+             resultxt = "Temprature: " + temp.ToString() + " deg \n" + "Pressure: " + pressure.ToString() + " Pa\n" + "Altitude: " + altitude.ToString() + "m" + "\n count: " + reportnumber.ToString();
+           
+
+            Task.Run(async () => { await AzureIoTHub.SendDeviceToCloudMessageAsync(reportnumber, temp, pressure); });
+
+        }
+
+
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+          
+            check();
+            result_txtBlock.Text = resultxt  ;
+
+        }
+    }
+}
